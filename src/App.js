898dd51f988eeacb216ab277335d108184adf92d
safouene-abbachi <safouene-abbachi@hotmail.fr>
2019-11-22 11:36:50 +0100

    added a spinner

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import MovieContainer from "./Components/MovieContainer";
 import "./App.css";
 import SearchMovie from "./Components/Search";
 import Ratings from "./Components/Ratings";
+import Withloading from "./Components/WithLoading";
+
+const Content = Withloading(MovieContainer);
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       searchWords: "",
-      star: ""
+      star: "",
+      loading: true
     };
   }
   searchHandler = event => this.setState({ searchWords: event.target.value });
   handlchange = newStar => this.setState({ star: newStar });
+
+  componentDidMount() {
+    setTimeout(() => this.setState({ loading: false }), 20000);
+  }
 
   render() {
     return (
@@ -26,7 +34,8 @@ class App extends Component {
         </div>
 
         <div>
-          <MovieContainer
+          <Content
+            isLoading={this.state.loading}
             rate={this.state.star}
             search={this.state.searchWords}
           />
